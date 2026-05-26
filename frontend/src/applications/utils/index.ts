ca5d61fee6cd7a0625486ec4d983b0e414ad67ec
fadhel <fadhelubain@gmail.com>
2026-05-26 08:04:54 +0300

    feat: add utility exports for game logic functions

---
+++ frontend/src/applications/utils/index.ts
@@ -0,0 +1,6 @@
+export * from "./checkTTT";
+export * from "./checkP4";
+export * from "./dropP4";
+export * from "./checkFanorona";
+export * from "./isPlacement";
+export * from "./fanoronaSuccessors";
